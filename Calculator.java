4f98d923e61985b6aa0d955a69bd76b925e92c5b
shishiruniyal-007 <shishir.uniyal@postman.com>
2026-06-23 11:23:59 +0530

    merged to single main

--- Calculator.java
+++ Calculator.java
@@ -17,12 +17,4 @@ public class Calculator {
         }
         return (double) a / b;
     }
-
-    public static void main(String[] args) {
-        Calculator calc = new Calculator();
-        System.out.println("2 + 3 = " + calc.add(2, 3));
-        System.out.println("5 - 1 = " + calc.subtract(5, 1));
-        System.out.println("4 * 6 = " + calc.multiply(4, 6));
-        System.out.println("10 / 2 = " + calc.divide(10, 2));
-    }
 }
